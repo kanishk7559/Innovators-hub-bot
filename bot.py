b1d034b90dde06ceb259064e1f4ac266b164336d
Kanishk Jamgaonkar <code.kanishk@gmail.com>
2021-03-27 18:38:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,83 @@
+import random
+from keras.models import load_model
+import keras
+import numpy as np
+import pickle
 import discord
-from discord.ext import commands
 import os
 import requests
 import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot_model.h5')
+TOKEN = ""
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+client = discord.Client()
 
 
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
+    return sentence_words
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
-client= discord.Client()
 
-def get_quote():
-	response= requests.get("https://zenquotes.io/api/random")
-	json_data = json.loads(response.text)
-	quote= json_data[0]['q'] + " \t\t\n-" + json_data[0]['a']
-	return(quote)
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print("found in bag: %s" % w)
+    return(np.array(bag))
 
-@client.event
-async def on_ready():
-	print("Working !")
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
 
-  msg = message.content
 
-  if msg.startswith('#inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
-  if msg.startswith('#help'):
-    quote = get_quote()
-    await message.channel.send("This Bot is under development")
-client.run("<< BOT TOKEN >>")
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag'] == tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+class MyClient(discord.Client):
+    async def on_message(self, message):
+
+        if message.author.id == self.user.id:
+            return
+        elif (message.content == 'quit'):
+            exit()
+        elif(message.content.startswith("%")):
+            msg = message.content
+            ints = predict_class(msg, model)
+            res = getResponse(ints, intents)
+            await message.channel.send(res.format(message))
+
+
+client = MyClient()
+client.run(TOKEN)
